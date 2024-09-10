824d979b68d3904d8e79c470b5529dd7560198ed
Leonardo Winckler <132357288+DraXx46@users.noreply.github.com>
2024-09-10 11:48:27 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+// JavaScript para abrir e fechar o modal
+function abrirModal() {
+    var modal = document.getElementById("modal");
+    modal.style.display = "block";
+}
+
+function fecharModal() {
+    var modal = document.getElementById("modal");
+    modal.style.display = "none";
+}
